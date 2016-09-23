7670f95adaa0b5d5e87c29fab7a85e668a2d1ce3
Giampiero <giampiero7@users.noreply.github.com>
2016-09-23 15:14:47 +0200

    version 1.0.1 - connection check
    - Added connection check
    - fixed bug for HTTP responses with no headers

--- src/main/java/cc/sferalabs/sfera/drivers/cameras/http_proxy/HttpProxyCamera.java
+++ src/main/java/cc/sferalabs/sfera/drivers/cameras/http_proxy/HttpProxyCamera.java
@@ -22,12 +22,15 @@
 
 package cc.sferalabs.sfera.drivers.cameras.http_proxy;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.eclipse.jetty.client.HttpClient;
 import org.eclipse.jetty.client.api.Response;
 import org.eclipse.jetty.client.util.InputStreamResponseListener;
 import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.http.HttpHeader;
 
 import cc.sferalabs.sfera.core.Configuration;
 import cc.sferalabs.sfera.drivers.Driver;
@@ -43,7 +46,9 @@ import cc.sferalabs.sfera.web.WebServerException;
  */
 public class HttpProxyCamera extends Driver {
 
+	private String url;
 	private CameraProxyServletHolder proxy;
+	private int errors;
 
 	public HttpProxyCamera(String id) {
 		super(id);
@@ -52,31 +57,31 @@ public class HttpProxyCamera extends Driver {
 	@Override
 	protected boolean onInit(Configuration config) throws InterruptedException {
 		try {
-			String url = config.get("url", null);
+			url = config.get("url", null);
 			if (url == null) {
 				throw new Exception("No URL specified");
 			}
 			String user = config.get("user", null);
 			String password = config.get("password", null);
 			String camPath = "/camera/" + getId();
-			String realm = getRealm(url);
+			String realm = getRealm();
 			proxy = new CameraProxyServletHolder(realm, user, password, url, camPath);
 			WebServer.addServlet(proxy, camPath + "/*");
+			errors = 0;
 			return true;
 
 		} catch (Exception e) {
-			log.error("Error initiating camera: " + e, e);
+			log.error("Error initializing camera: " + e, e);
 			return false;
 		}
 	}
 
 	/**
 	 * 
-	 * @param url
 	 * @return
 	 * @throws Exception
 	 */
-	private String getRealm(String url) throws Exception {
+	private String getRealm() throws Exception {
 		HttpClient client = new HttpClient();
 		try {
 			client.start();
@@ -84,13 +89,13 @@ public class HttpProxyCamera extends Driver {
 			client.newRequest(url).send(listener);
 			Response response = listener.get(10, TimeUnit.SECONDS);
 			for (HttpField h : response.getHeaders()) {
-				if (h.getHeader().is("WWW-Authenticate")) {
+				HttpHeader header = h.getHeader();
+				if (header != null && header.is("WWW-Authenticate")) {
 					for (String val : h.getValues()) {
 						String[] r = val.split("=");
 						if (r.length == 2 && r[0].trim().equalsIgnoreCase("Basic realm")) {
 							String realm = r[1].trim();
 							realm = realm.substring(1, realm.length() - 1);
-							System.err.println(realm);
 							return realm;
 						}
 					}
@@ -106,9 +111,44 @@ public class HttpProxyCamera extends Driver {
 
 	@Override
 	protected boolean loop() throws InterruptedException {
-		// TODO check connection to camera is still on...
-		Thread.sleep(5000000);
-		return true;
+		Thread.sleep(30000);
+
+		try {
+			if (!checkConnection()) {
+				if (++errors > 4) {
+					log.warn("Many connection errors");
+					return false;
+				}
+			}
+			return true;
+		} catch (Exception e) {
+			log.error("Loop error", e);
+			return false;
+		}
+	}
+
+	/**
+	 * 
+	 * @return
+	 * @throws ExecutionException
+	 * @throws InterruptedException
+	 * @throws Exception
+	 */
+	private boolean checkConnection() throws Exception {
+		HttpClient client = new HttpClient();
+		try {
+			client.start();
+			InputStreamResponseListener listener = new InputStreamResponseListener();
+			client.newRequest(url).send(listener);
+			listener.get(5, TimeUnit.SECONDS);
+			return true;
+
+		} catch (TimeoutException | ExecutionException e) {
+			log.debug("Connection error");
+			return false;
+		} finally {
+			client.stop();
+		}
 	}
 
 	@Override
